56aa54fb2d5dd95a0bfe772ca54e852a8cd60718
barbara42 <barbara.raven42@gmail.com>
2016-10-28 01:45:25 -0400

    documentation for cfgable

--- src/edu/mit/compilers/cfg/CFGAble.java
+++ src/edu/mit/compilers/cfg/CFGAble.java
@@ -10,5 +10,11 @@ import edu.mit.compilers.lowir.instructions.Instruction;
 public interface CFGAble {
     public abstract CFG generateCFG(CFGContext context);
     public abstract void cfgPrint(PrintWriter pw, String prefix);
+    
+    /*
+     * -every generateAssembly that allocates a register should also deallocate the register
+     * -every generateAssembly function should use `ctx.pushStack` to push a copy of itself 
+     *  	onto the stack for future expressions that want to use it's value
+     */
     public List<Instruction> generateAssembly(AssemblyContext ctx);
 }
