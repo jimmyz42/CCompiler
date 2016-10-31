e6b54697b595efca04f8bc6879e578dda6305f25
barbara42 <barbara.raven42@gmail.com>
2016-10-30 23:38:35 -0400

    changed Cmove printout from cmovel to cmove

--- src/edu/mit/compilers/lowir/instructions/Cmove.java
+++ src/edu/mit/compilers/lowir/instructions/Cmove.java
@@ -18,7 +18,7 @@ public class Cmove extends Instruction {
 
 	@Override
 	public void printAssembly(PrintWriter pw, String prefix) {
-        pw.print(prefix + "cmovel");
+        pw.print(prefix + "cmove");
         src.printAssembly(pw, " ");
         dest.printAssembly(pw, ", ");
         pw.println();
