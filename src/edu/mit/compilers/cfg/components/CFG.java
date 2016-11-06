05385e5ec40183d659bf240cde62778bb9e573e2
barbara42 <barbara.raven42@gmail.com>
2016-11-05 23:47:04 -0400

    changed 2 to -2 for exit status whooops

--- src/edu/mit/compilers/cfg/components/CFG.java
+++ src/edu/mit/compilers/cfg/components/CFG.java
@@ -414,7 +414,7 @@ public class CFG implements CFGAble {
         // Fall Off Method Handler
         ctx.addInstruction(Label.create("fall_off_error"));
         ctx.addInstruction(Mov.create(ImmediateValue.create(60), Register.create("%rax")));
-        ctx.addInstruction(Mov.create(ImmediateValue.create(2), Register.create("%rdi")));
+        ctx.addInstruction(Mov.create(ImmediateValue.create(-2), Register.create("%rdi")));
         ctx.addInstruction(Syscall.create());
     }
 
