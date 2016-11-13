2a32de7d0c5a4e148877dc66bbd4efade39ff361
Jimmy <jimmy@dhcp-18-189-33-52.dyn.mit.edu>
2016-11-13 15:16:18 -0500

    Add optimize package/class

---
+++ src/edu/mit/compilers/optimize/Optimizations.java
@@ -0,0 +1,13 @@
+package edu.mit.compilers.optimize;
+import edu.mit.compilers.cfg.components.BasicBlock;
+import java.util.List;
+
+public class Optimizations {
+	public List<BasicBlock> run(List<BasicBlock> blocks) {
+		// TODO implement
+		// run the different optimizations
+		return null;
+	}
+	
+	// TODO put the optimization methods here
+}
