76335cd1d029e96980446b75ef02641e846135a9
Jimmy <jimmy@dhcp-18-189-42-190.dyn.MIT.EDU>
2016-11-19 00:53:11 -0500

    fix NPE for return statement

--- src/edu/mit/compilers/highir/nodes/ReturnStmt.java
+++ src/edu/mit/compilers/highir/nodes/ReturnStmt.java
@@ -130,7 +130,9 @@ public class ReturnStmt extends Statement implements Optimizable {
 	
 	@Override
 	public Optimizable algebraSimplify() {
-		this.expression = (Expression)expression.algebraSimplify();
+		if(expression != null) {
+			this.expression = (Expression)expression.algebraSimplify();
+		}
 		return this;
 	}
 }
