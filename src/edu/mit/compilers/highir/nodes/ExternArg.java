436b38fe34508c3bf204b2e987df51a2e062f932
barbara42 <barbara.raven42@gmail.com>
2016-12-04 14:10:11 -0500

    Revert "Revert "reaching defs done but NEEDS testing""
    This reverts commit bd55f3f94ffbd52317b9a94f7fa030c07e1bdeec.

--- src/edu/mit/compilers/highir/nodes/ExternArg.java
+++ src/edu/mit/compilers/highir/nodes/ExternArg.java
@@ -58,6 +58,16 @@ abstract public class ExternArg extends Ir implements Storable, CFGAble, Optimiz
     public void generateAssembly(AssemblyContext ctx) {
     	
     }
+
+    @Override
+    public void numberDefinitions(OptimizerContext ctx){
+        
+    }
+
+    @Override
+    public void findVarToDefs(OptimizerContext ctx){
+        
+    }
     
     @Override
     public long getNumStackAllocations() {
